2bdc8e17bb4566ae71b16e5cefa7c02e2c77672e
yskandre <54952885+yskandre@users.noreply.github.com>
2020-12-07 01:29:10 +0100

    Initial Setup

---
+++ hephaistos.js
@@ -0,0 +1,37 @@
+const { app } = require('electron')
+const AspectRatioBrowserWindow = require('electron-aspect-ratio-browser-window')
+
+function createWindow() {
+  // Create the browser window.
+  const win = new AspectRatioBrowserWindow({
+    show: false,
+    frame: false,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemoteModule: true,
+    },
+  })
+
+  win.setAspectRatio(16 / 9)
+  win.maximize()
+  win.show()
+
+  // and load the index.html of the app.
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
