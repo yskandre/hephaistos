f37972bcb2386b9928ec4df3fc0b8c75d87bf4c8
yskandre <54952885+yskandre@users.noreply.github.com>
2021-01-13 02:55:06 +0100

    Added some more functionality to courses

--- js/courses.js
+++ js/courses.js
@@ -1,24 +1,43 @@
 data = JSON.parse(fs.readFileSync(path.join('assets', 'lessons.json')))
 
-lessons = document.getElementById('lessontab')
-details = document.getElementById('detailtab')
-console.log(data)
-
 data.forEach((s) => {
-  segment = document.createElement('div')
-  header = document.createElement('span')
-  header.innerHTML = s.name
-  segment.appendChild(header)
-  console.log(s.lessons)
-  s.lessons.forEach((l) => {
-    lesson = document.createElement('button')
-    lesson.className += 'lessonbutton'
-    lesson.innerHTML = l.name
-    lesson.addEventListener('click', function () {
-      details.innerHTML = l.writeup
+  segment = $('<div/>')
+  header = $(`<span>${s.name}</span><br/>`)
+  segment.append(header)
+  s.lessons.forEach((lesson) => {
+    button = $(`<button class='lesson-button'>${lesson.name}</button>`)
+    button.on('click', (e) => {
+      if ($(e.target).hasClass('selected')) {
+        $(e.target).removeClass('selected')
+        $('#lesson-info').text('poopie')
+        console.log(lesson.name + '_2')
+      } else {
+        $('.lesson-button').removeClass('selected')
+        $(e.target).addClass('selected')
+        $('#lesson-info').text(lesson.writeup)
+        console.log(lesson.name + '_1')
+      }
     })
-    segment.appendChild(document.createElement('br'))
-    segment.appendChild(lesson)
+    button.appendTo(segment)
+    segment.append($('<br/>'))
   })
-  lessons.appendChild(segment)
+  $('#lesson-tab').append(segment)
 })
+
+function loadLesson(questions) {
+  //Create Modal
+  _.sample(questions, Math.min(questions.length / 2, 5)).forEach((q) => {
+    switch (q.type) {
+      //Create Question and append to modal as a tab
+      case 'match':
+        //TODO
+        break
+      case 'single choice':
+        //TODO
+        break
+      case 'translate':
+        //TODO
+        break
+    }
+  })
+}
--- js/user.js
+++ js/user.js
@@ -1,9 +1,11 @@
 const fs = require('fs')
 const path = require('path')
+const $ = require('jquery')
+const _ = require('underscore')
 
 data = JSON.parse(fs.readFileSync(path.join('assets', 'userdata.json')))
 
-document.getElementById('userimg').src = path.join(...data.image)
-document.getElementById('username').innerHTML = data.name
-document.getElementById('title').innerHTML = data.title
-document.getElementById('xp').innerHTML = data.xp + 'XP gesammelt'
+$('#user-img').attr('src', path.join(...data.image))
+$('#user-name').html(data.name)
+$('#title').html(data.title)
+$('#xp').html(data.xp + 'XP gesammelt')
