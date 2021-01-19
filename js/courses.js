22270915f989633a166770e26aaf97aadf60a295
yskandre <54952885+yskandre@users.noreply.github.com>
2021-01-19 02:43:08 +0100

    functionality added for choice

--- js/courses.js
+++ js/courses.js
@@ -9,6 +9,7 @@ data.forEach((s) => {
     button.on('click', (e) => {
       if ($(e.target).hasClass('selected')) {
         $(e.target).removeClass('selected')
+        loadLesson(lesson.questions)
         $('#lesson-info').text('poopie')
         console.log(lesson.name + '_2')
       } else {
@@ -25,19 +26,124 @@ data.forEach((s) => {
 })
 
 function loadLesson(questions) {
+  console.log('existing')
+  modal = $(`<div>Gaming</div>`)
+  //add win div
   //Create Modal
-  _.sample(questions, Math.min(questions.length / 2, 5)).forEach((q) => {
+  let match = []
+  let build = []
+  let choice = ''
+  _.sample(questions, Math.min(questions.length, 5)).forEach(function (q) {
+    question = $(`<div class="question">${q.question}<br></div>`)
     switch (q.type) {
-      //Create Question and append to modal as a tab
       case 'match':
-        //TODO
+        left = _.shuffle(q.options[0])
+        right = _.shuffle(q.options[1])
+        for (let i = 0; i < Math.min(left.length, right.length); i++) {
+          buttonLeft = $(`<button>${left[i]}</button>`)
+
+          buttonRight = $(`<button>${right[i]}</button>`)
+          //mark pressed buttons, unmark by clicking again
+          //if 2 buttons are pressed, if combination is in q.solutions, +XP
+          //if correct, disable the buttons, if wrong just unmark them
+          //if all buttons are disabled, create button to go next
+          buttonLeft.appendTo(question)
+          buttonRight.appendTo(question)
+          $('<br>').appendTo(question)
+        }
+        /*
+        submit = $(`<button>Abgeben!</button>`)
+        submit.on('click', () => {
+          q.solutions.every((s) => {
+            if (_.isEqual(s, build)) {
+              xp += 5
+              //indicator
+              return false
+            }
+            return true
+          })
+
+          if (i < modal.children().toArray().length - 1) {
+            next = $(`<button>Nächste Aufgabe</button>`)
+          } else {
+            next = $(`<button>Beenden</button>`)
+          }
+          next.on('click', () => {
+            modal.children().each((idx, e) => {
+              if (idx === i) {
+                e.show()
+              } else {
+                e.hide()
+              }
+            })
+          })
+          next.appendTo(question)
+          $(this).hide()
+        })
+        submit.appendTo(question)
+        */
         break
-      case 'single choice':
-        //TODO
+      case 'build':
+        options = _.shuffle(q.options)
+        options.forEach((e) => {
+          button = $(`<button>${e}</button>`)
+          //onclick add to build array or remove again ORDER AUTOMATICALLY
+          button.appendTo(question)
+          $('<br>').appendTo(question)
+        })
+        submit = $(`<button>Abgeben!</button>`)
+        submit.on('click', function () {
+          q.solutions.every((s) => {
+            if (_.isEqual(s, build)) {
+              xp += 5
+              //indicator
+              return false
+            }
+            return true
+          })
+
+          next = $(`<button>Super</button>`)
+          next.on('click', function () {
+            $(this).parent().prev().toggle()
+            $(this).parent().toggle()
+          })
+          next.appendTo($(this).parent())
+          $(this).toggle()
+        })
+        submit.appendTo(question)
         break
-      case 'translate':
-        //TODO
+      case 'choice':
+        options = _.shuffle(q.options)
+        options.forEach((e) => {
+          button = $(`<button>${e}</button>`)
+          button.on('click', () => {
+            choice = e
+          })
+          button.appendTo(question)
+          $('<br>').appendTo(question)
+        })
+        submit = $(`<button>Abgeben!</button>`)
+        submit.on('click', function () {
+          if (_.contains(q.solutions, choice)) {
+            xp += 5
+            console.log(xp)
+            //indicator
+          }
+
+          next = $(`<button>Super</button>`)
+          next.on('click', function () {
+            $(this).parent().prev().toggle()
+            $(this).parent().toggle()
+          })
+          next.appendTo($(this).parent())
+          $(this).toggle()
+        })
+        submit.appendTo(question)
         break
     }
+    question.appendTo(modal)
+    question.toggle()
   })
+  modal.children().last('.question').toggle()
+  modal.appendTo('#lesson-tab')
 }
--- js/user.js
+++ js/user.js
@@ -4,8 +4,14 @@ const $ = require('jquery')
 const _ = require('underscore')
 
 data = JSON.parse(fs.readFileSync(path.join('assets', 'userdata.json')))
+xp = data.xp
+title = data.title
 
 $('#user-img').attr('src', path.join(...data.image))
 $('#user-name').html(data.name)
-$('#title').html(data.title)
-$('#xp').html(data.xp + 'XP gesammelt')
+updateUser()
+
+function updateUser() {
+  $('#title').html(title)
+  $('#xp').html(xp + 'XP gesammelt')
+}
